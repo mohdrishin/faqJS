222a6f807d3963c20d04d02e5813f909a23f4bce
rishin <mrishinv@gmail.com>
2021-06-26 23:20:16 +0530

    traversing DOM js code added

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 
+// traversing the DOM
 
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function (btn){
+    btn.addEventListener("click", function(event){
+        const question = event.currentTarget.parentElement.parentElement;
+        question.classList.toggle("show-text");
+    });
+});
 
